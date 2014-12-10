1371fb63d8dac83f3b039c36844ad0ccfef96185
Jon Schlueter <jon.schlueter@gmail.com>
2014-12-10 05:55:10 -0500

    Another pass at build_option_testing script
    expanded which flags where being tested
    dropped it back to minimal format
    added an always on (minimal)
    remove the .scons-options-cache before every run to prevent
    cross-contamination
    remove running the regression tests
    added -j9 build option to speed things up

--- build_option_testing.py
+++ build_option_testing.py
@@ -1,31 +1,74 @@
 
+import os
+
+always_on = [
+    'minimal',
+]
+
+always_off = [
+    'debug',
+    'chrpath',
+    'ipv6',
+    'manbuild',
+    'leapfetch',
+    'nostrip',
+    'slow',
+    'profiling',
+    'libQgpsmm',
+]
+
 knobs = [
-    'nmea0183',
+    'reconfigure',
+    'aivdm',
     'ashtech',
+    'bluez',
+    'clientdebug',
+    'control_socket',
+    'controlsend',
+    'coveraging',
+    'dbus_export',
     'earthmate',
     'evermore',
+    'force_global',
+    'fury',
     'fv18',
     'garmin',
     'garmintxt',
     'geostar',
+    'gpsclock',
     'itrax',
+    'libgpsmm',
     'mtk3301',
     'navcom',
+    'ncurses',
+    'netfeed',
+    'nmea0183',
+    'nmea2000',
+    'nofloats',
+    'ntpshm',
+    'ntrip',
+    'oceanserver',
+    'oldstyle',
     'oncore',
+    'passthrough',
+    'pps',
+    'python',
+    'qt',
+    'rtcm104v2',
+    'rtcm104v3',
+    'shared',
+    'shm_export',
     'sirf',
+    'socket_export',
+    'squelch',
     'superstar2',
+    'systemd',
+    'timing',
     'tnt',
     'tripmate',
     'tsip',
     'ublox',
-    'fury',
-    'nmea2000',
-    'aivdm',
-    'gpsclock',
-    'ntrip',
-    'oceanserver',
-    'rtcm104v2',
-    'rtcm104v3',
+    'usb',
 ]
 
 import itertools
@@ -36,16 +79,22 @@ for i in range(0, len(knobs)):
 
     for row in list(jj):
         params = []
+
+        for key in always_on:
+            params.append(key+"=on")
+
         for key in knobs:
             if key in row:
                 params.append(key+"=on")
-            else:
-                params.append(key+"=off")
+
         #print {'on_params': row, 'scons_params': params}
 
         import subprocess
-        command = ['scons', 'testregress']
+        command = ['scons', '-j9']
         command.extend(params)
+        if os.path.exists('.scons-option-cache'):
+            os.remove('.scons-option-cache')
+
         retval = subprocess.call(command)
         if retval != 0:
             failed_configurations.append(command)
