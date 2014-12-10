02e5669cb2430cf2523f480b02a38b0da6fa396a
Jon Schlueter <jon.schlueter@gmail.com>
2014-12-09 22:15:02 -0500

    Add build_option_testing script
    attempt to try the different permutations of build options to see
    what is working and what is broken

---
+++ build_option_testing.py
@@ -0,0 +1,57 @@
+
+knobs = [
+    'nmea0183',
+    'ashtech',
+    'earthmate',
+    'evermore',
+    'fv18',
+    'garmin',
+    'garmintxt',
+    'geostar',
+    'itrax',
+    'mtk3301',
+    'navcom',
+    'oncore',
+    'sirf',
+    'superstar2',
+    'tnt',
+    'tripmate',
+    'tsip',
+    'ublox',
+    'fury',
+    'nmea2000',
+    'aivdm',
+    'gpsclock',
+    'ntrip',
+    'oceanserver',
+    'rtcm104v2',
+    'rtcm104v3',
+]
+
+import itertools
+failed_configurations = []
+
+for i in range(0, len(knobs)):
+    jj = itertools.combinations(knobs, i)
+
+    for row in list(jj):
+        params = []
+        for key in knobs:
+            if key in row:
+                params.append(key+"=on")
+            else:
+                params.append(key+"=off")
+        #print {'on_params': row, 'scons_params': params}
+
+        import subprocess
+        command = ['scons', 'testregress']
+        command.extend(params)
+        retval = subprocess.call(command)
+        if retval != 0:
+            failed_configurations.append(command)
+            print command
+            with open('failed_build_configs.txt', 'a') as failed_configs:
+                failed_configs.write(command + '\n')
+
+for row in failed_configurations:
+    print ' '.join(row)
