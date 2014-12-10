f2f657ade3844ef2f440e2d6edc511249d534044
Jon Schlueter <jon.schlueter@gmail.com>
2014-12-09 22:02:32 -0500

    whitespace cleanup in test_maidenhead.py

--- test_maidenhead.py
+++ test_maidenhead.py
@@ -12,11 +12,11 @@ import sys
 
 errors = 0
 for (lat, lon, maidenhead, location) in [
-    (48.86471,  2.37305, "JN18eu", "Paris"),
-    (41.93498, 12.43652, "JN61fw", "Rome"),
-    (39.9771,  -75.1685, "FM29jx", "Philadelphia"),
-    (-23.4028, -50.9766, "GG46mo", "Sao Paulo"),
-    ]:
+        (48.86471,  2.37305, "JN18eu", "Paris"),
+        (41.93498, 12.43652, "JN61fw", "Rome"),
+        (39.9771,  -75.1685, "FM29jx", "Philadelphia"),
+        (-23.4028, -50.9766, "GG46mo", "Sao Paulo"),
+]:
     converted = gps.clienthelpers.maidenhead(lat, lon)
     if converted != maidenhead:
         print >>sys.stderr, "maidenhead test: from %s %s (%s) expected %s got %s" \
